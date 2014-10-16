b4702e601dd9fbc1e7e02ecc23403f8aa6144820
Casey Allred <casey@modernizedmedia.com>
2014-10-15 19:42:40 -0600

    macro fizzbuzz

--- test_fizzbuzz.rs
+++ test_fizzbuzz.rs
@@ -1,103 +1,51 @@
-// fn main() {
-//     for i in range(1i, 101) {
-//         if i % 15 == 0 {
-//             println!("FizzBuzz");
-//         } else if i % 5 == 0 {
-//             println!("Buzz");
-//         } else if i % 3 == 0 {
-//             println!("Fizz");
-//         } else {
-//             println!("{}", i);
-//         }
-//     }
-// }
+//rustc --pretty expanded test_fizzbuzz.rs
+//rustc test_fizzbuzz.rs && ./test_fizzbuzz
 
-
-
-
-// use std::rc::Rc;
-
-// fn test(n: int, d: int, s: &str, prev: String) -> String { 
-//     if n % d == 0{
-//         s.to_string() + prev
-//     } else {
-//         prev
-//     }
-// }
-
-/// Implements the FizzBuzz algorithm.
-/// If n is:
-///     * divisible by 3, returns "fizz"
-///     * divisible by 5, returns "buzz"
-///     * divisible by 3 and 5, returns "fizzbuzz"
-///     * anything else, returns n as a String
-fn fizzbuzz(n: int) -> String {
-
-    fn test(n: int, d: int, s: &str, prev: String) -> String { 
-        if n % d == 0{
-            s.to_string() + prev
-        } else {
-            prev
+#![feature(macro_rules)]
+macro_rules! fizzbuzz(
+    ($n:ident $(($d:expr, $str:expr)).+ ) => (
+        {
+            let n: int = $n;
+            let test = |d: int, s: &str, prev: String| -> String {
+                if n % d == 0 {
+                    s.to_string() + if prev == "".to_string() { prev } else { "_".to_string() + prev }
+                } else {
+                    prev
+                }
+            };
+            let x = "".to_string();
+            $(
+                let x = test($d, $str, x);
+            )+
+            if x == "".to_string() {
+                n.to_string()
+            } else {
+                x
+            }
         }
-    }
+    );
+)
 
-    // fn test(d: int, s: &str, prev: String) -> String { 
-    //     if n % d == 0{
-    //         s.to_string() + prev
-    //     } else {
-    //         prev
-    //     }
-    // }
-
-    // let test = |d, s: &str, prev: String| -> String
-    //                      if n % d == 0 {
-    //                          s.to_string() + prev
-    //                      } else {
-    //                          prev
-    //                      };
-
-    // let x = test(3, "fizz", test(5, "buzz", "".to_string()) );
-
-
-    // let testRc = Rc::new( |d, s: &str, prev: String| -> String
-    //                      if n % d == 0 {
-    //                          s.to_string() + prev
-    //                      } else {
-    //                          prev
-    //                      }
-    //                      );
-    // let test3 = testRc.clone();
-    // let test5 = testRc.clone();
-    // let x = (*test3)(3, "fizz", (*test5)(5, "buzz", "".to_string()) );
-
-    // let x2 = test(5, "buzz", "".to_string());
-    // let x = test(3, "fizz", x2);
-
-    // let x = test(3, "fizz", test(5, "buzz", "".to_string()) );
-
-    let x = test(n, 3, "fizz", test(n, 5, "buzz", "".to_string()));
-
-    if x == "".to_string() {
-        n.to_string()
-    } else {
-        x
-    }
-
+fn fizzbuzz(n: int) -> String {
+    fizzbuzz!(n (11,"pappa").(8,"lulz").(5,"buzz").(3,"fizz"))
 }
 
 fn main() {
-    // assert!(fizzbuzz(3).as_slice() == "fizz");
-    // assert!(fizzbuzz(5).as_slice() == "buzz");
-    // assert!(fizzbuzz(15).as_slice() == "fizzbuzz");
-    // assert!(fizzbuzz(7).as_slice() == "7");
-    // assert!(fizzbuzz(8).as_slice() == "lulz");
+    assert!(fizzbuzz(3).as_slice() == "fizz");
+    assert!(fizzbuzz(5).as_slice() == "buzz");
+    assert!(fizzbuzz(15).as_slice() == "fizz_buzz");
+    assert!(fizzbuzz(7).as_slice() == "7");
 
     // for i in range(1, 16) {
-        // println!("{}", fizzbuzz(i));
+    //     println!("{}", fizzbuzz(i));
     // }
 
-    for f in range(1, 10001).map(fizzbuzz) {
-        println!("{}", f);
-    }
+    // for i in range(1, 101) {
+    //     println!("{}", fizzbuzz(i));
+    // }
+
+    // for f in range(1, 10001).map(fizzbuzz) {
+        // println!("{}", f);
+    // }
 
 }
